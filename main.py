2a6cc90764dd7c8af28a8f7a184ef3d03e3f4557
Matvei Naumov <mat-naumov@yandex.ru>
2026-02-06 10:29:52 +0400

    init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+if api_key == None:
+    raise RuntimeError("Something wrong with you API key...")
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
+
