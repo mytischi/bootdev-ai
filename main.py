c92800228e3173af8ab2c77e141d3f6c7d306724
Matvei Naumov <mat-naumov@yandex.ru>
2026-02-06 12:34:27 +0400

    add CL input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 load_dotenv()
@@ -11,12 +12,20 @@ client = genai.Client(api_key=api_key)
 if api_key == None:
     raise RuntimeError("Something wrong with you API key...")
 
+
+parser = argparse.ArgumentParser(description="bootdev-ai")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model='gemini-2.5-flash', contents=args.user_prompt
 )
 
 if response.usage_metadata == None:
     raise RuntimeError("Something went wrong...")
+
+
 
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
