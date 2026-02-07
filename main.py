6968e6812a707435b669e398d54291fe85bbcfea
Matvei Naumov <mat-naumov@yandex.ru>
2026-02-07 23:34:46 +0400

    add verbose expanded output toggle

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ if api_key == None:
 
 parser = argparse.ArgumentParser(description="bootdev-ai")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -25,12 +26,15 @@ response = client.models.generate_content(
     model='gemini-2.5-flash', contents=messages
 )
 
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 if response.usage_metadata == None:
     raise RuntimeError("Something went wrong...")
 
 
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
 
