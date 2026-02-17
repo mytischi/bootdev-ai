4dc1f59bd017ccf1f36c8cd5d1d9a3cf338c14d2
Matvei Naumov <naumov.mytischi@gmail.com>
2026-02-17 11:17:23 +0400

    implement LLM rules

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,15 +1,54 @@
+import os
+import subprocess
+
 def run_python_file(working_directory, file_path, args=None):
 
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+    try:
 
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
 
-    if not valid_target_dir:
-        return print(f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory')
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
-    if not os.path.isfile(target_dir):
-        return print(f'Error: "{file_path}" does not exist or is not a regular file')
+        if not valid_target_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
 
-    if not "target_dir".endswith(.py):
-        return print(f'Error: "{file_path}" is not a Python file')
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+
+        if not target_dir.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+
+        command = ["python", target_dir]
+
+        if args:
+            command.extend(args)
+
+        result = subprocess.run(
+                command, 
+                cwd=working_directory, 
+                capture_output=True, 
+                text=True, 
+                timeout=30
+                )
+
+        output = ""
+        
+        if result.returncode != 0:
+            output = f"Process exited with code {result.returncode}\n"
+
+        if not result.stdout and not result.stderr:
+            output += "No output produced"
+
+        if result.stdout:
+            output += f"STDOUT:\n{result.stdout}"
+
+        if result.stderr:
+            output += f"STDERR:\n{result.stderr}"
+    
+        return output
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
